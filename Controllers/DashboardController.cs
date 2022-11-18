3c6ca29819e0cbfba6aa5466bbb45f8561dd8fc7
Nguyen Ngoc Tien <64886876+tientulac@users.noreply.github.com>
2022-11-18 17:57:34 +0700

    update dashboard

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -14,6 +14,20 @@ namespace QLXeBuyt.Controllers
         // GET: DashBoard
         public ActionResult Index()
         {
+            var totalIncome = 0;
+            ViewBag.CountKhachHang = db.Khachhangs.Count();
+            ViewBag.CountTuyenXe = db.Tuyenxes.Count();
+            ViewBag.CountXe = db.Xes.Count();
+            ViewBag.CountHoaDon = db.CT_Hoadons.Count();
+            ViewBag.ListTuyenXe = db.Tuyenxes.ToList();
+            var lstMaVe = db.CT_Hoadons.Select(x => x.Mave).Distinct();
+            lstMaVe.ToList().ForEach(x => { 
+                if (db.VeXes.Select(v => v.Mave).ToList().Contains(x))
+                {
+                    totalIncome += (int)db.VeXes.Where(m => m.Mave == x).FirstOrDefault().Giatien;
+                }
+            });
+            ViewBag.TotalIncome = totalIncome;
             return View();
         }
 
